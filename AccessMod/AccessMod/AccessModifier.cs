75c0f259b013a14a1dc4c6bff8668cf34a37441d
“Niketanjenekar” <niketanjenekar31@gmail.com>
2023-06-15 09:21:00 +0530

    New Concept about modifiers

---
+++ AccessMod/AccessMod/AccessModifier.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessMod
+{
+    //Acesss Modifier are the keywords which specifies the accessibility or scope of a variable or a function.
+    //it will control the visibility of a class.
+    public class AccessModifier
+    {
+        //Public Modifier allow a part of a program in the same assembly or a different assembly to access the type and 
+        public void Modifer()
+        {
+            Console.WriteLine("This is Public Modifier");
+        }
+
+        //Private Modifier it resticate other program part from accessing its members.
+        private void PrivModifier()
+        {
+            Console.WriteLine("This is a Private Modifier");
+            
+        }
+        public void demo()
+        {
+            PrivModifier();
+        }
+
+        //Protected Modifier allows the code withiin the same class and inherited class and its member.
+        protected void ProtModifier()
+        {
+            Console.WriteLine("This is a Protected Modifier");
+            
+        }
+    }
+}
---
+++ AccessMod/AccessMod/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessMod
+{
+    internal class Program:ProtectedModifier
+    {
+        static void Main(string[] args)
+        {
+            AccessModifier accessModifier = new AccessModifier();
+            accessModifier.Modifer();
+            Console.ReadLine();
+
+            PrivateModifier privateModifier = new PrivateModifier();
+            privateModifier.demo();
+            Console.ReadLine();
+
+            Program program = new Program();
+            program.ProtModifier();
+            Console.ReadLine();
+
+            InternalModifier internalModifier = new InternalModifier();
+            internalModifier.InternMod();
+        }
+    }
+}
